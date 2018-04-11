d237f06ac90b9d503314ec62e62e4f1cba3896e7
gaoleMeng <gaole@umich.edu>
2018-04-10 23:40:51 -0400

    lazy commit

--- largeScaleGraph/cpp/generate_first.cpp
+++ largeScaleGraph/cpp/generate_first.cpp
@@ -29,7 +29,7 @@ int main() {
     for (string dir: dir_list) {
         for (auto & p : fs::directory_iterator(dir)) {
             // ifstream ifs(p);
-            thread_list.push_back(thread t1(read_and_parse, p));
+            thread_list.push_back(thread(read_and_parse, p));
         }
     }
 
