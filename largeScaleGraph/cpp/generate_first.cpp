91d89905c48a4c2154362a343f5090326d122df7
gaoleMeng <gaole@umich.edu>
2018-04-10 23:00:52 -0400

    lazy commit

--- largeScaleGraph/cpp/generate_first.cpp
+++ largeScaleGraph/cpp/generate_first.cpp
@@ -9,20 +9,21 @@
 using namespace std;
 namespace fs = boost::filesystem;
 
-string input_dir_1 = "/scratch/si699w18_fluxm/gaole/aminer_papers_0";
-string input_dir_2 = "/scratch/si699w18_fluxm/gaole/aminer_papers_1";
-string input_dir_3 = "/scratch/si699w18_fluxm/gaole/aminer_papers_2";
-vector<string> dir_list = {input_dir_1, input_dir_2, input_dir_3};
+path input_dir_1("/scratch/si699w18_fluxm/gaole/aminer_papers_0");
+path input_dir_2("/scratch/si699w18_fluxm/gaole/aminer_papers_1");
+path input_dir_3("/scratch/si699w18_fluxm/gaole/aminer_papers_2");
+vector<path> dir_list = {input_dir_1, input_dir_2, input_dir_3};
 
 int main() {
 
-    for (string dir: dir_list) {
+    for (path dir: dir_list) {
 
-        std::vector<directory_entry> v;
-        copy(directory_iterator(dir), directory_iterator(), back_inserter(v));
-        for ( auto it = v.begin(); it != v.end(); ++it)
+        directory_iterator end_itr;
+
+        for (directory_iterator itr(dir); it != end_itr; ++it)
         {
-            std::cout<< (*it).path().string()<<endl;
+            string current_file = itr->path().string();
+            cout << current_file << endl;
         }
     }
 }
