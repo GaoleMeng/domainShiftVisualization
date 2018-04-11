4aa3c5f6d726ddedfbe13f69fe9cf11d77443bc7
gaoleMeng <gaole@umich.edu>
2018-04-10 23:09:35 -0400

    lazy commit

--- largeScaleGraph/cpp/generate_first.cpp
+++ largeScaleGraph/cpp/generate_first.cpp
@@ -6,28 +6,19 @@
 #include <string>
 #include <experimental/filesystem>
 // #include <boost/range/iterator_range.hpp>
-
+namespace fs = std::filesystem;
 using namespace std;
 // using namespace boost::filesystem;
 
-path input_dir_1("/scratch/si699w18_fluxm/gaole/aminer_papers_0");
-path input_dir_2("/scratch/si699w18_fluxm/gaole/aminer_papers_1");
-path input_dir_3("/scratch/si699w18_fluxm/gaole/aminer_papers_2");
-vector<path> dir_list = {input_dir_1, input_dir_2, input_dir_3};
+string input_dir_1 = "/scratch/si699w18_fluxm/gaole/aminer_papers_0";
+string input_dir_2 = "/scratch/si699w18_fluxm/gaole/aminer_papers_1";
+string input_dir_3 = "/scratch/si699w18_fluxm/gaole/aminer_papers_2";
+vector<string> dir_list = {input_dir_1, input_dir_2, input_dir_3};
 
 int main() {
 
-    for (path dir: dir_list) {
-        cout << "dd" << endl;
-
-        // directory_iterator end_itr;
-
-        // for (directory_iterator itr(dir); itr != end_itr; ++itr)
-        // {
-        //     string current_file = itr->path().string();
-        //     cout << current_file << endl;
-        // }
-    }
+    for (auto & p : fs::directory_iterator(input_dir_1))
+        std::cout << p << std::endl;
 }
 
 
