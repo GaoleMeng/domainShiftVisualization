d4caa9196ec16c35745e053c69c4ae872091646c
gaoleMeng <gaole@umich.edu>
2018-04-11 20:11:19 -0400

    lazy commit

--- largeScaleGraph/cpp/generate_second.cpp
+++ largeScaleGraph/cpp/generate_second.cpp
@@ -58,10 +58,10 @@ void read_and_parse(int indices) {
             if (regex_search(line, venue_extract, venue)) {
                 string id_string = string(id_extract[0]).substr(7, 24);
                 // if (!string_pool.count(id_string)) continue;
-
+                cout << line << endl;
                 cout << "lao de" << endl;
                 cout << string(venue_extract[0]) << endl;
-                cout << line << endl;
+                
                 string reference_string =  string(venue_extract[0]).substr(10, string(venue_extract[0]).length() - 11);
                 cout << reference_string << endl;
                 string refer_string = "";
