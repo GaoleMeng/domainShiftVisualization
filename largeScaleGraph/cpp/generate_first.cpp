b43414e085473e40e583cf5025cc171b35e747f5
gaoleMeng <gaole@umich.edu>
2018-04-11 22:48:38 -0400

    lazy commit

--- largeScaleGraph/cpp/generate_first.cpp
+++ largeScaleGraph/cpp/generate_first.cpp
@@ -50,7 +50,7 @@ void read_and_parse(int indices) {
             smatch venue_extract;
             if (regex_search(line, venue_extract, venue)) {
                 string reference_string = "";
-                // if (venue_extract[0] != "\"venue\": \"SIGIR\"") continue;
+                if (venue_extract[0] != "\"venue\": \"SIGIR\"") continue;
                 
                 string id_string = string(id_extract[0]).substr(7, 24);
                 string refer_string = "";
