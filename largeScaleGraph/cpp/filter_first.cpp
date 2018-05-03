9d08810deb023fdfc533970b771056a98e1509bd
gaoleMeng <gaole@umich.edu>
2018-05-03 11:14:30 +0800

    lazy commit

--- largeScaleGraph/cpp/filter_first.cpp
+++ largeScaleGraph/cpp/filter_first.cpp
@@ -28,7 +28,7 @@ string first_layer = "/scratch/si699w18_fluxm/gaole/cpp_largevis_first.txt";
 string second_layer = "/scratch/si699w18_fluxm/gaole/cpp_largevis_second.txt";
 string third_layer = "/scratch/si699w18_fluxm/gaole/cpp_largevis_third.txt";
 string index_file_output = "/scratch/si699w18_fluxm/gaole/index_file.txt";
-string output_file = "/scratch/si699w18_fluxm/gaole/lines_belong_toconf.txt";
+string output_file = "/scratch/si699w18_fluxm/gaole/lines_belong_toconf_ss.txt";
 
 vector<string> dir_list = {input_dir_1, input_dir_2, input_dir_3};
 vector<string> layer_file_list = {first_layer, second_layer, third_layer};
@@ -90,7 +90,7 @@ void read_and_parse(int indices) {
                 if (regex_search(line, year_extract, year)) {
 
                     venue_string = string(venue_extract[0]).substr(10, venue_extract[0].length() - 11);
-                    if (string_pool.count(venue_string) || issubstr(venue_string, "SIGIR")) {
+                    if (string_pool.count(venue_string)) {
                         output_lock.lock();
                         output << line << "\n";
                         output_lock.unlock();
