5d78fa6a5de906f993212b4ebd37ea31864cefff
gaoleMeng <gaole@umich.edu>
2018-05-03 08:28:19 +0800

    lazy commit

--- largeScaleGraph/cpp/filter_second.cpp
+++ largeScaleGraph/cpp/filter_second.cpp
@@ -147,6 +147,7 @@ void generate_final_conf(unordered_map<string, int> mapping_file) {
 
     for (const auto& tmpp: mapping_file) {
         // cout << (double) bfs_index_map[tmpp.first] / (double) tmpp.second << endl;
+        cout <<"SIF " <<bfs_index_map["SIGIR"] << endl;
         if ((double) bfs_index_map[tmpp.first] / (double) tmpp.second > ratio_thres) {
             // tmp.push_back({tmpp.first, tmpp.second});
             final_conf.insert(tmpp.first);
