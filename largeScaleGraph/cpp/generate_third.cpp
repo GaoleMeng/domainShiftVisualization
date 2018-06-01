3a114644ee7f3a114f4234459bf91edfe8b80f31
gaoleMeng <gaole@umich.edu>
2018-06-02 01:01:32 +0800

    lazy commit

--- largeScaleGraph/cpp/generate_third.cpp
+++ largeScaleGraph/cpp/generate_third.cpp
@@ -22,17 +22,17 @@ using namespace rapidjson;
 
 
 // Congiguration: input aminer directory
-string input_dir_1 = "/scratch/si699w18_fluxm/gaole/aminer_papers_0";
-string input_dir_2 = "/scratch/si699w18_fluxm/gaole/aminer_papers_1";
-string input_dir_3 = "/scratch/si699w18_fluxm/gaole/aminer_papers_2";
+string input_dir_1 = "/home/wuzhuofeng/raw_data/";
+// string input_dir_2 = "/scratch/si699w18_fluxm/gaole/aminer_papers_1";
+// string input_dir_3 = "/scratch/si699w18_fluxm/gaole/aminer_papers_2";
 
 // Configuration: input comes from the output of the second layer
-string input_lastlayer = "/scratch/si699w18_fluxm/gaole/cpp_largevis_second.txt";
+string input_lastlayer = "/home/wuzhuofeng/raw_data/cpp_largevis_second.txt";
 
 // Configuration: output the final layer o
-string output_file = "/scratch/si699w18_fluxm/gaole/cpp_largevis_third.txt";
+string output_file = "/home/wuzhuofeng/raw_data/cpp_largevis_third.txt";
 
-vector<string> dir_list = {input_dir_1, input_dir_2, input_dir_3};
+vector<string> dir_list = {input_dir_1;
 unordered_set<string> string_pool;
 
 string lastfix = ".txt";
