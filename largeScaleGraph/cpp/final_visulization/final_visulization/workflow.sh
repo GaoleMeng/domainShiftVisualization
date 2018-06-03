331db581cb92e48810e10ce750054e8729b0ca34
gaoleMeng <gaole@umich.edu>
2018-06-03 20:41:55 +0800

    lazy commit

--- largeScaleGraph/cpp/final_visulization/final_visulization/workflow.sh
+++
@@ -1,5 +0,0 @@
-python plot.py -input 1_points.txt -out 1.png -label 1_labels.txt
- python plot.py -input 2_points.txt -out 2.png -label 2_labels.txt
- python plot.py -input 3_points.txt -out 3.png -label 3_labels.txt
-# python plot.py -input 3_point.txt -out 3.png -label 3_label.txt
-
---
+++ largeScaleGraph/cpp/generate_final_input.py
@@ -0,0 +1,45 @@
+"""
+the python version of "generate final input"
+which takes the filtered file and output the edges computation
+or it can read the final output of large vis and output the author point split
+and paper embedding split
+"""
+
+import os
+import sys
+import json
+
+# the 
+input_dir_1 = "/home/wuzhuofeng/intermediate_files/lines_belong_toconf_smaller.txt";
+
+output_file = "/home/wuzhuofeng/intermediate_files/non_bias_edges_withauthors.txt";
+
+largeVis_output = "./citation_qiaozhu.txt";
+
+split_location = "/home/wuzhuofeng/domainShiftVisualization/largeScaleGraph/cpp/final_visulization/";
+
+conf_lines_file = open(input_dir_1)
+
+
+
+
+def read_and_parse():
+    for line in conf_lines_file:
+        tmp_obj = json.loads(line)
+        print(tmp_obj)
+        break
+        
+        
+        
+
+
+def main():
+    read_and_parse()
+    
+    
+
+
+def __name__ == "__main__":
+    main()
+
+
