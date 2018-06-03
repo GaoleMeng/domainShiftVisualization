aff1aab39a4558865621a6e1dc51b523930cc7a6
gaoleMeng <gaole@umich.edu>
2018-06-03 21:36:30 +0800

    lazy commit

--- largeScaleGraph/cpp/generate_final_input.py
+++ largeScaleGraph/cpp/generate_final_input.py
@@ -8,6 +8,7 @@ and paper embedding split
 import os
 import sys
 import json
+import sets
 
 # the 
 input_dir_1 = "/home/wuzhuofeng/intermediate_files/lines_belong_toconf_smaller.txt";
@@ -27,6 +28,9 @@ index_count = 0
 id_to_ref = {}
 id_to_index = {}
 year_to_indexlist = {}
+year_counter = {}
+author_to_index = {}
+conf_pool = set()
 
 
 def read_and_parse():
@@ -39,9 +43,10 @@ def read_and_parse():
             continue
         if "year" not in tmp_obj:
             continue
-        venue_string = tmp_obj["venue"]
+        venue_list = tmp_obj["venue"]
+        conf_pool.add(venue_list)
+
         id_string = tmp_obj["id"]
-        author_list = tmp_obj["authors"]
         year_string = tmp_obj["year"]
         if year_string not in year_to_indexlist:
             year_to_indexlist[year_string] = []
@@ -51,11 +56,19 @@ def read_and_parse():
         if id_string not in id_to_ref:
             id_to_ref[id_string] = []
 
+        if "authors" in tmp_obj:
+            author_list = tmp_obj["authors"]
+            for tmp in author_list:
+                if "name" in tmp:
+                    if tmp["name"] not in author_to_index:
+                        author_to_index[tmp["name"]] = []
+                    author_to_index[tmp["name"]].append(index_count)
+
         if "references" in tmp_obj:
             for ref in tmp_obj["references"]:
                 for tmp in ref:
                     id_to_ref[id_string].append(tmp)
-        
+
         year_to_indexlist[year_string].append(index_count)
         index_count += 1
 
