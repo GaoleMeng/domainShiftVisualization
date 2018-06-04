f46eaa4330ee48cd0ff4f68c3c6a50f82ee12eb6
gaoleMeng <gaole@umich.edu>
2018-06-04 15:40:14 +0800

    lazy commit

--- largeScaleGraph/cpp/generate_final_input.py
+++ largeScaleGraph/cpp/generate_final_input.py
@@ -165,6 +165,8 @@ def generate_files():
             elementX = 0.0
             elementY = 0.0
             counter = 0
+
+            print(author_dict)
             for k, v in author_dict.items():
                 if str(k) > split_points[i] and str(k) < split_points[i+1]: 
                     for index in v:
