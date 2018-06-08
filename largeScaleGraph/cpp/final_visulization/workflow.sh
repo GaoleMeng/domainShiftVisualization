b6dcc09fe5594c384800c4d83a4c9a0d1c4b176f
gaoleMeng <gaole@umich.edu>
2018-06-08 13:32:12 +0800

    lazy commit

--- largeScaleGraph/cpp/final_visulization/workflow.sh
+++ largeScaleGraph/cpp/final_visulization/workflow.sh
@@ -5,5 +5,3 @@ python plot.py -input 1_points.txt -out 1.png -label 1_labels.txt
 python plot.py -input 0_authors.txt -out author1.png 
 python plot.py -input 1_authors.txt -out author2.png 
 python plot.py -input 2_authors.txt -out author3.png 
-
-python generate_table.py -input 2_authors.txt -out table.png 
