6cc888be86739fa9c29f50465d6b0ae8dc747669
gaoleMeng <gaole@umich.edu>
2018-06-13 23:14:01 +0800

    lazy commit

--- largeScaleGraph/cpp/generate_final_input.py
+++ largeScaleGraph/cpp/generate_final_input.py
@@ -407,9 +407,9 @@ def create_class_map():
 
     for k, v in all_conf:
         color_map[k] = eq_name_map[v]
-    
+
     # print(color_map)
-    print(eq_name_map)
+    # print(eq_name_map)
     print("unique conf:" + str(len(eq_name_map)))
     class_file.close()
     
