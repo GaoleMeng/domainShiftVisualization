536615b87d896097e78b53f05d2836924c9ebc83
gaoleMeng <gaole@umich.edu>
2018-09-09 22:24:21 -0700

    new

--- largeScaleGraph/cpp/generate_final_input.py
+++ largeScaleGraph/cpp/generate_final_input.py
@@ -15,13 +15,13 @@ import csv
 
 
 # the
-input_dir_1 = "/home/wuzhuofeng/lines_belong_toconf_smaller.txt";
+input_dir_1 = "/home/zhuofeng/lines_belong_toconf_smaller.txt";
 
-output_file = "/home/wuzhuofeng/non_bias_edges_withauthors.txt";
+output_file = "/home/zhuofeng/non_bias_edges_withauthors.txt";
 
 largeVis_output = "./citation_qiaozhu.txt";
 
-split_location = "/home/wuzhuofeng/domainShiftVisualization/largeScaleGraph/cpp/final_visulization/";
+split_location = "./final_visulization/";
 
 class_map_file = "./class_map.txt"
 
